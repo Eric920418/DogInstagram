75186e1f3dd79bc3bc8b0867064fd9222edab72f
eric <26416387.re@gmail.com>
2024-11-19 11:07:40 +0800

    Initial Commit

---
+++ DogInstagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DogInstagram
+//
+//  Created by eric on 2024/11/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ DogInstagramTests/DogInstagramTests.swift
@@ -0,0 +1,17 @@
+//
+//  DogInstagramTests.swift
+//  DogInstagramTests
+//
+//  Created by eric on 2024/11/19.
+//
+
+import Testing
+@testable import DogInstagram
+
+struct DogInstagramTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
